1b0e55ad2cf6e3a98361173460206cb8211fabc3
samirjkhadka <findsamirkhadka@gmail.com>
2025-01-28 16:58:15 +0545

    project setup complete header section complete mobile Nav complete

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <div className="w-full overflow-hidden">
+        <Header />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import { assets } from "../assets/assets";
+import { useEffect } from "react";
+
+const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  useEffect(() => {
+    if (showMobileMenu) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [showMobileMenu]);
+
+  return (
+    <div className="absolute top-0 left-0 w-full z-10">
+      <div className="container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent">
+        <img src={assets.logo} alt="" className="" />
+        <ul className="hidden md:flex gap-7 text-white">
+          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+            Home
+          </a>
+          <a href="#About" className="cursor-pointer hover:text-gray-400">
+            About
+          </a>
+          <a href="#Projects" className="cursor-pointer hover:text-gray-400">
+            Projects
+          </a>
+          <a
+            href="#Testimonials"
+            className="cursor-pointer hover:text-gray-400"
+          >
+            Testimonials
+          </a>
+        </ul>
+        <img
+          onClick={() => setShowMobileMenu(true)}
+          src={assets.menu_icon}
+          alt=""
+          className="md:hidden w-7 cursor-pointer"
+        />
+      </div>
+      {/* Mobile Screen */}
+      <div
+        className={`md:hidden ${
+          showMobileMenu ? "fixed w-full" : "h-0 w-0"
+        } fixed w-full right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}
+      >
+        <div className="flex justify-end p-6 cursor-pointer">
+          <img
+            src={assets.cross_icon}
+            alt=""
+            className="w-6"
+            onClick={() => setShowMobileMenu(false)}
+          />
+        </div>
+        <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+          <a
+            href=""
+            className="px-4 py-2 rounded-full inline-block"
+            id="#Header"
+            onClick={() => setShowMobileMenu(false)}
+          >
+            Home
+          </a>
+          <a
+            href=""
+            className="px-4 py-2 rounded-full inline-block"
+            id="#About"
+            onClick={() => setShowMobileMenu(false)}
+          >
+            About
+          </a>
+          <a
+            href=""
+            className="px-4 py-2 rounded-full inline-block"
+            id="#Projects"
+            onClick={() => setShowMobileMenu(false)}
+          >
+            Projects
+          </a>
+          <a
+            href=""
+            className="px-4 py-2 rounded-full inline-block"
+            id="#Testimonials"
+            onClick={() => setShowMobileMenu(false)}
+          >
+            Testimonials
+          </a>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
