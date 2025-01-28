57c3d9b71ab373520d6267ce62b4b005d8bd0242
samirjkhadka <findsamirkhadka@gmail.com>
2025-01-28 20:48:05 +0545

    made nav bar sticky

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   }, [showMobileMenu]);
 
   return (
-    <div className="absolute top-0 left-0 w-full z-10">
+    <div className="absolute top-0 left-0 w-full z-10 fixed">
       <div className="container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent">
         <img src={assets.logo} alt="" className="" />
         <ul className="hidden md:flex gap-7 text-white">
